a6466ef3f4e101f9a75bc22591e66a851b9d8e45
윤수빈 <subin0910@gachon.ac.kr>
2024-11-17 21:10:42 +0900

    Fix output format and precision

--- tester.py
+++ tester.py
@@ -1,9 +1,11 @@
 import geo.utils as utils
 
+# Pythagoras example
 a, b = 3, 4
 c = utils.pythagoras(a, b)
-print('c=', c)
+print(f"c = {c:.1f}")  # 소수점 1자리까지 출력
 
+# Circle area example
 r = 10
 area = utils.circle(r)
-print('area=', area)
+print(f"area = {area:.15f}")  # 소수점 15자리까지 출력
